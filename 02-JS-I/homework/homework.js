055d9a9034622e795783b7ad26f581901e52a46a
FlechaMartin <martin.eduard.martin@gmail.com>
2022-02-18 09:56:05 -0300

    Segundo_commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -6,18 +6,18 @@ const nuevaString = "Martin";
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 5;
 
+
 // Crea una variable booleana:
 const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 -  5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
-
+const nuevoModulo = 21 % 5 === 1;
 
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
@@ -28,6 +28,7 @@ const nuevoModulo = 21 % 5 === null;
 function devolverString(str) {
   // "Return" la string provista: str
   // Tu código:
+    return str;
   
 }
 
@@ -35,32 +36,37 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+    var sum = x + y;
+    return sum;
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+    var rest= x - y;
+    return rest;  
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+    var mul = x * y;
+    return mul;  
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+    var div = x / y;
+    return div;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+    var igual= x == y;
+    return igual;
 }
 
 function tienenMismaLongitud(str1, str2) {
